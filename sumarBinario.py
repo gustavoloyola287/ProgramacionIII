5d96e21eb78807d52407525673c9c55802e9af51
gustavoloyola <gustavoloyola287@gmail.com>
2025-04-09 09:24:45 -0300

    primer commit

---
+++ sumarBinario.py
@@ -0,0 +1,28 @@
+
+#Código
+#Caso de prueba
+#Resultado de la prueba
+#Resultado de la prueba
+#67. Sumar binario
+#Fácil
+#Temas
+#Empresas
+#Dadas dos cadenas binarias ay b, devuelve su suma como una cadena binaria .
+
+ 
+
+#Ejemplo 1:
+
+#Entrada: a = "11", b = "1"
+# Salida: "100"
+#Ejemplo 2:
+
+#Entrada: a = "1010", b = "1011"
+# Salida: "10101"
+ 
+
+#Restricciones:
+
+#1 <= a.length, b.length <= 104
+#a y b constan únicamente de caracteres '0'o .'1'
+#Cada cadena no contiene ceros iniciales, excepto el cero mismo.
